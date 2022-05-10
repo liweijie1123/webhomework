8baed5920af7787475facd52e1f5a58f2766fd97
Liweijie <1771662778@qq.com>
2022-05-10 21:45:46 +0800

    实验十一完成

--- project11/js/main.js
+++ project11/js/main.js
@@ -1,8 +1,46 @@
+const DDC = document
+let addClEvt = (el,cb)=>{el.addEventListener('click',cb)}
+let getEle   = (e,all=false)=>{return (all)? DDC.querySelectorAll(`${e}`):DDC.querySelector(`${e}`)}
+var i = 1;
+var length= getEle('.mm').length;
+var a = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
 function myFunction(x) {
   if (x.matches) { // 媒体查询
       document.querySelector(".m-trg").addEventListener('click',()=>{
-        document.querySelector(".m-nav").classList.add('mobile')
+        if(i==1){
+        document.querySelector(".m-nav").classList.add('mobile');
+          getEle('.m-trg').querySelectorAll('span')[2].style.display='none'
+          getEle('.m-trg').querySelectorAll('span')[1].style.transform = "rotate(45deg)";
+          getEle('.m-trg').querySelectorAll('span')[0].style.transform = "rotate(-45deg)";
+          getEle('.m-trg').querySelectorAll('span')[0].style.margin ="-6px 0"; 
+
+
+        i=i-1;
+        }
+        else
+        {
+          document.querySelector(".m-nav").classList.remove('mobile');
+          getEle('.m-trg').querySelectorAll('span')[2].style.display='block'
+          getEle('.m-trg').querySelectorAll('span')[1].style.transform = "rotate(0deg)";
+          getEle('.m-trg').querySelectorAll('span')[0].style.transform = "rotate(0deg)";
+          getEle('.m-trg').querySelectorAll('span')[0].style.margin ="0"; 
+          i = i+1;
+        }
       })
+    getEle('.mm',true).forEach((item,i)=>{
+      addClEvt(item,()=>{
+        if(a[i]==1){
+        getEle('.mob',true)[i].style.display='flex';
+        a[i]=a[i]-1;
+        }
+        else
+        {
+          getEle('.mob',true)[i].style.display='none';
+          a[i]=a[i]+1;
+        }
+        
+      })
+    })
   }
 }
 
